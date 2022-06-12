e32532bd4affb5a1b10b4f37b6dff62a3c8a6ea0
Korisnik <crvenioklop@gmail.com>
2022-06-12 23:51:58 +0200

    add portals for modal and backdrop

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,11 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import style from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
 const ErrorModal = (props) => {
-  return (
-    <>
-      <div className={style.backdrop} onClick={props.onConfirm} />
+  const Backdrop = (props) => {
+    return <div className={style.backdrop} onClick={props.onConfirm} />;
+  };
+
+  const ModalOverlay = (props) => {
+    return (
       <Card className={style.modal}>
         <header className={style.header}>
           <h2>{props.title}</h2>
@@ -18,6 +22,23 @@ const ErrorModal = (props) => {
           <Button click={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
+    );
+  };
+
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          info={props.info}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
